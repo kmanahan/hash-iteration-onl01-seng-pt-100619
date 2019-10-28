d2925081717452926dc1c4658ec20d55770a6d42
Kelly Manahan <kelly.a.manahan@gmail.com>
2019-10-28 18:31:52 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -14,9 +14,9 @@ end
 def age_appropriate_birthday(birthday_kids)
     birthday_kids.each.map do |kids_name, age|
     if age <= 12
-      "Happy Birthday #{kids_name}! You are now #{age} years old!"
+      puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
     else 
-    "Happy Birthday #{kids_name}! You are now #{age} years old!"
+    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
   end 
 end 
 end
